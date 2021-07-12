6c45efe35cace47f7d93ad2ecefdcddb9d792688
GitOmen <86669185+GitOmen@users.noreply.github.com>
2021-07-12 03:05:05 +0200

    refactor: add error handlers

--- main.py
+++ main.py
@@ -18,6 +18,16 @@ def build_response(body=None, status=HTTPStatus.OK):
     return Response(response, status=status, mimetype=MIME)
 
 
+@app.errorhandler(services.TaskDoesNotExistException)
+def handle_task_does_not_exist(exc):
+    return build_response({'error': str(exc)}, HTTPStatus.NOT_FOUND)
+
+
+@app.errorhandler(services.InvalidStatusException)
+def handle_invalid_status(exc):
+    return build_response({'error': str(exc)}, HTTPStatus.BAD_REQUEST)
+
+
 @app.route('/')
 def hello_world():
     return build_response("It's LightToDoApp!")
@@ -26,44 +36,29 @@ def hello_world():
 @app.route("/tasks", methods=['POST'])
 def add_task():
     request_data = request.get_json()
-    try:
-        return build_response(services.add_task(request_data))
-    except services.InvalidStatusException as exc:
-        return build_response({'error': str(exc)}, HTTPStatus.BAD_REQUEST)
+    return build_response(services.add_task(request_data))
 
 
 @app.route('/tasks')
 def get_all_tasks():
-
     return build_response(services.get_all_tasks())
 
 
 @app.route('/tasks/<int:id>')
 def get_task(id):
-    try:
-        return build_response(services.get_task(id))
-    except services.TaskDoesNotExistException as exc:
-        return build_response({'error': str(exc)}, HTTPStatus.NOT_FOUND)
+    return build_response(services.get_task(id))
 
 
 @app.route('/tasks/<int:id>', methods=['PATCH'])
 def update_task(id):
     request_data = request.get_json()
-    try:
-        return build_response(services.update_task(id, request_data))
-    except services.TaskDoesNotExistException as exc:
-        return build_response({'error': str(exc)}, HTTPStatus.NOT_FOUND)
-    except services.InvalidStatusException as exc:
-        return build_response({'error': str(exc)}, HTTPStatus.BAD_REQUEST)
+    return build_response(services.update_task(id, request_data))
 
 
 @app.route('/tasks/<int:id>', methods=['DELETE'])
 def delete_task(id):
-    try:
-        services.delete_task(id)
-        return build_response(status=HTTPStatus.NO_CONTENT)
-    except services.TaskDoesNotExistException as exc:
-        return build_response({'error': str(exc)}, HTTPStatus.NOT_FOUND)
+    services.delete_task(id)
+    return build_response(status=HTTPStatus.NO_CONTENT)
 
 
 if __name__ == '__main__':
